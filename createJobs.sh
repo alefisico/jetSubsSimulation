a99f114b83caecf0bbf04f6a6d0c0d04f3fa3dc2
Alejandro Gomez Espinosa <alefisico@gmail.com>
2014-04-24 19:30:54 -0400

    fix bug

--- createJobs.sh
+++ createJobs.sh
@@ -25,40 +25,48 @@
 
 stop1=100
 stop2=250											# Mass of the stop2
+numJobs=200
+run=0
 #foreach Process ("jj") # "bj")									# Here I have two different final states (Maybe you dont need it)
 
 Base_Dir=/cms/gomez/Substructure/Generation/Simulation/CMSSW_5_3_2_patch4/src/			# Dir from where we have to load the enviroment
 Main_Dir=${Base_Dir}jetSubsSimulation/								# Main Dir
 #Name=${stop2}St2toSt1Z_${stop1}RPVSttojj_8TeV_8TeVPU						# Name of the process
-Name=RPVSt${stop1}tojj_8TeV_HT500_0			 						# Name of the process
-Working_Dir=${Main_Dir}/${Name}									# Working directory
+Name=RPVSt${stop1}tojj_8TeV_HT500		 						# Name of the process
 Output_Dir=/home/gomez/mySpace/Stops/st_jj/AOD/${Name}/						# Output directory
+LHE_File_Dir=/cms/gomez/Substructure/Generation/MG5v1.5.11/RPVSttojj/Events/RPVSttojj_0/	# Directory of the lhe file.
+LHE_Name=unweighted_events_fixed.lhe
 emailForNotifications=gomez@physics.rutgers.edu
-numJobs=200
 #hadronizer=Hadronizer_MgmMatchTune4C_7TeV_madgraph_pythia8_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_RECO_PU.py
-hadronizer=templates/Hadronizer_TuneD6T_8TeV_madgraph_tauola_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_RECO_PU.py
+hadronizer=templates/Hadronizer_TuneD6T_8TeV_madgraph_tauola_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_L1Reco_RECO_PU_1.py
 
 #####################################################
-#### Here is where the code starts.. Do NOT change
+#### Here is where the code starts.. 
 #####################################################
 
 echo " Creating directories..."
 ####### Working directory
+Working_Dir=${Main_Dir}/${Name}	
 if [ -d $Working_Dir ]; then
-	rm -rf $Working_Dir
-	mkdir -p $Working_Dir 
+	rm -rf $Working_Dir/${Name}_${run}
+	mkdir -p $Working_Dir/${Name}_${run}
 else
-	mkdir -p $Working_Dir 
+	mkdir -p $Working_Dir/${Name}_${run} 
 fi
-cd $Working_Dir
+cd $Working_Dir/${Name}_${run}
 
 ####### EOS directory for root files
 if [ -d $Output_Dir ]; then
 	rm -rf $Output_Dir
-	mkdir -p $Output_Dir 
+	mkdir -p $Output_Dir/${Name}_${run}/lhe/
+	mkdir -p $Output_Dir/${Name}_${run}/aodsim/
 else
-	mkdir -p $Output_Dir 
+	mkdir -p $Output_Dir/${Name}_${run}/lhe/
+	mkdir -p $Output_Dir/${Name}_${run}/aodsim/
 fi
+
+######## Create a symbolic link to the lhe file
+ln -s ${LHE_File_Dir}/${LHE_Name} ${Output_Dir}/${Name}_${run}/lhe/${Name}_${run}.lhe
 
 ##############################################
 ##### Create the python file for Ntuples
@@ -97,7 +105,7 @@ eval `scramv1 runtime -sh`
 
 cd $CUR_DIR
 
-cmsRun '${namePythonFile}' $1 $2 $3 $4 '>> ${nameRunFile}
+cmsRun '${namePythonFile}' $1 $2 $3 $4 $5 '>> ${nameRunFile}
 
 chmod +x ${nameRunFile}
 
@@ -110,17 +118,17 @@ if [ -f $nameCondorFile ]; then
 	rm -rf $nameCondorFile
 fi
 echo "universe = vanilla
-Executable = ${Working_Dir}/${nameRunFile}
+Executable = ${Working_Dir}/${Name}_${run}/${nameRunFile}
 Notify_User = ${emailForNotifications}" >> ${nameCondorFile}
 
-for ((version=0;version<${numJobs};version++))
+for ((version=1;version<${numJobs}+1;version++))
 do
-	events=$(echo "1+(${version})*500" | bc)
+	events=$(echo "1+(${version}-1)*500" | bc)
 	echo "
-Output = ${Working_Dir}/${Name}_${version}.stdout
-Error = ${Working_Dir}/${Name}_${version}.stderr
-Log = ${Working_Dir}/${Name}_${version}.condorlog
-Arguments = ${Name} ${version} 1 ${events} ${Output_Dir}
+Output = ${Working_Dir}/${Name}_${run}/${Name}_${version}.stdout
+Error = ${Working_Dir}/${Name}_${run}/${Name}_${version}.stderr
+Log = ${Working_Dir}/${Name}_${run}/${Name}_${version}.condorlog
+Arguments = ${Name}_${run} 1 ${version} ${events} ${Output_Dir}
 Queue ">> ${nameCondorFile}
 done
 
